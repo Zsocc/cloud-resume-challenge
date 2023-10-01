4ef9878f0028f9a6adad8a37bb282b1c79771d90
Zsocc <szabozsolti890212@gmail.com>
2023-10-01 11:59:50 +0200

    fetch modified

--- Frontend/cv.js
+++ Frontend/cv.js
@@ -1,6 +1,6 @@
 
 //fetch('https://ekq1gza932.execute-api.us-east-1.amazonaws.com/Test/getVisitorCount')
-fetch('https://dm01y76po7.execute-api.us-east-1.amazonaws.com/')
+fetch('https://efpic5j5fl.execute-api.us-east-1.amazonaws.com/')
 .then(res => res.json())
 .then(res => {document.getElementById("counter").innerHTML = res.count})
 .then(data => console.log(data));
