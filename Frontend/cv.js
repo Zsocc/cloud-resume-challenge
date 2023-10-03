6d1cb0fb3b81a1661bd904c1f90091a89270246e
Zsocc <szabozsolti890212@gmail.com>
2023-10-03 09:55:38 +0200

    fetch updated with new API id

--- Frontend/cv.js
+++ Frontend/cv.js
@@ -1,6 +1,6 @@
 
 //fetch('https://ekq1gza932.execute-api.us-east-1.amazonaws.com/Test/getVisitorCount')
-fetch('https://efpic5j5fl.execute-api.us-east-1.amazonaws.com/')
+fetch('https://i8jzd7yev5.execute-api.us-east-1.amazonaws.com/')
 .then(res => res.json())
 .then(res => {document.getElementById("counter").innerHTML = res.count})
 .then(data => console.log(data));
