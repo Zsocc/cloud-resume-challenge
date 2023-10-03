b8dc731a7620722512a7dd80f3d601c73d163a49
Zsocc <szabozsolti890212@gmail.com>
2023-10-03 10:37:22 +0200

    copied providers into Backend, updated javascript

--- Frontend/cv.js
+++ Frontend/cv.js
@@ -1,5 +1,4 @@
 
-//fetch('https://ekq1gza932.execute-api.us-east-1.amazonaws.com/Test/getVisitorCount')
 fetch('https://i8jzd7yev5.execute-api.us-east-1.amazonaws.com/')
 .then(res => res.json())
 .then(res => {document.getElementById("counter").innerHTML = res.count})
