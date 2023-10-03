2f4ca746a077a922eee22db09f34f1b4a2061b06
Zsocc <szabozsolti890212@gmail.com>
2023-10-03 13:25:58 +0200

    updated cv.html and cv.js

--- Frontend/cv.js
+++ Frontend/cv.js
@@ -1,5 +1,5 @@
 
-fetch('https://i8jzd7yev5.execute-api.us-east-1.amazonaws.com/')
+fetch('https://huqqk3iqia.execute-api.us-east-1.amazonaws.com/')
 .then(res => res.json())
 .then(res => {document.getElementById("counter").innerHTML = res.count})
 .then(data => console.log(data));
